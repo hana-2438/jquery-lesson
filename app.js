115372e6a04948defbf2152056411f0da989b3bf
hana-2438 <121591703+hana-2438@users.noreply.github.com>
2023-03-30 01:22:57 +0000

    [start]jquery6

---
+++ app.js
@@ -0,0 +1,47 @@
+// $(document).ready(function (){
+//   $('body').html('<h1>HELLO jQuery!!</h1>');
+// });
+
+// $(function(){
+//   $('.box1').css({
+//     'background-color' : '#0000FF',
+//     'height' : '100px'
+//   })
+// })
+
+// $(function(){
+//   $('.box1').slideDown(function (){
+//   $('.box1').css({
+//     'background-color' : '#0000FF',
+//     'width' : '200px',
+//     'height' : '100px'
+//   }).slideUp();
+//   });
+// });
+
+// $(function(){
+//   $('.box1').show();
+//   $('.box1').css({'background-color' : '#0000FF'});
+// });
+
+// $(function(){
+//   $('.box1').hide();
+// });
+
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
+
+$(function(){
+  $('.box1').on('click', function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
+  });
+});
