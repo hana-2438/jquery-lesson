fe5fff24a439671f27447679cf4bda1ce69ad6ac
hana-2438 <121591703+hana-2438@users.noreply.github.com>
2023-03-30 01:47:35 +0000

    [add]jquery7-this

--- app.js
+++ app.js
@@ -1,47 +1,25 @@
-// $(document).ready(function (){
-//   $('body').html('<h1>HELLO jQuery!!</h1>');
-// });
-
-// $(function(){
-//   $('.box1').css({
-//     'background-color' : '#0000FF',
-//     'height' : '100px'
-//   })
-// })
-
-// $(function(){
-//   $('.box1').slideDown(function (){
-//   $('.box1').css({
-//     'background-color' : '#0000FF',
-//     'width' : '200px',
-//     'height' : '100px'
-//   }).slideUp();
-//   });
-// });
-
-// $(function(){
-//   $('.box1').show();
-//   $('.box1').css({'background-color' : '#0000FF'});
-// });
-
-// $(function(){
-//   $('.box1').hide();
-// });
-
-// $(function(){
-//   $('.box1').mouseover(function(){
-//     $('.box1').addClass('box1-ext');
-//   });
-//   $('.box1').mouseout(function(){
-//     $('.box1').removeClass('box1-ext');
-//   });
-// });
-
+// thisを使って記述を省略したバージョン
 $(function(){
   $('.box1').on('click', function(){
-    $('.box1').addClass('box1-ext');
-  });
-  $('.box1').mouseout(function(){
-    $('.box1').removeClass('box1-ext');
+    $(this).slideUp();
   });
 });
+
+// 省略せずに記述したバージョン
+$(function(){
+  $('.bg1').on('click', function(){
+    $('.bg1').slideUp();
+  });
+
+  $('.bg2').on('click', function(){
+    $('.bg2').slideUp();
+  });
+
+  $('.bg3').on('click', function(){
+    $('.bg3').slideUp();
+  });
+
+  $('.bg4').on('click', function(){
+    $('.bg4').slideUp();
+  });
+})
