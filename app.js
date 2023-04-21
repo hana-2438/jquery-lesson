1397048ee64563f9f02ece5fe98acbc9f945c639
hana-2438 <hana-2438@users.noreply.github.com>
2023-04-21 15:20:13 +0900

    [finish]jquery-7

--- app.js
+++ app.js
@@ -1,25 +1,5 @@
-// thisを使って記述を省略したバージョン
 $(function(){
-  $('.box1').on('click', function(){
-    $(this).slideUp();
+  $('button').on('click', function(){
+    $('ul').children().css('color','red');
   });
 });
-
-// 省略せずに記述したバージョン
-$(function(){
-  $('.bg1').on('click', function(){
-    $('.bg1').slideUp();
-  });
-
-  $('.bg2').on('click', function(){
-    $('.bg2').slideUp();
-  });
-
-  $('.bg3').on('click', function(){
-    $('.bg3').slideUp();
-  });
-
-  $('.bg4').on('click', function(){
-    $('.bg4').slideUp();
-  });
-})
